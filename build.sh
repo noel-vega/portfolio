0cde081c9775cf0db6af6b1c49c7ca00993485db
Noel Vega <noelvegajr94@gmail.com>
2025-12-14 12:20:40 -0500

    build script.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Clean previous build
+rm -rf dist
+
+# Compile Typescript
+tsc
+
+# Copy static assets
+npx @tailwindcss/cli -i ./src/input.css -o ./dist/public/css/main.css
+cp -r src/public dist
+
+echo "Build completed"
